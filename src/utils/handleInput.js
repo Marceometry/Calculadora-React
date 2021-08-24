5e9b1d88b617d2ad98a633c385a7c78fe66fc09c
Marcelino <marceometry@gmail.com>
2021-08-24 13:57:06 -0300

    improve handleInput function

--- src/utils/handleInput.js
+++ src/utils/handleInput.js
@@ -1,15 +1,12 @@
 export function handleInput(value, currentValue) {    
     if (!value) return
 
-    const isNan = isNaN(Number(value))
+    const isNan = value => { return isNaN(Number(value)) }
     const lastCharacter = currentValue[currentValue.length - 1]
 
-    if (isNan) {
-        if (lastCharacter === ' ' || lastCharacter === '.') return
-
-        if (value !== '.') {
-            return [...currentValue, ' ', value, ' ']
-        }
+    if (isNan(value)) {
+        if (isNan(lastCharacter) || lastCharacter === ' ') return
+        if (value !== '.') return [...currentValue, ' ', value, ' ']
     }
 
     return [...currentValue, value]
